4895363599983487533fd80ebd3e3ecae3b8aa76
Aaron Colwell <acolwell@google.com>
2012-11-27 16:15:18 -0800

    Applying changes from TPAC discussions.
    Bug 18709 - Added transition to HAVE_METADATA when current playback position is removed.
    Bug 18587 - Add remove() calls to duration change algorithm.
    Bug 19531 - Added MediaSource.isTypeSupported() method.
    Bug 18601 - Remove initialization segments are optional text.

--- media-source.js
+++ media-source.js
@@ -128,6 +128,7 @@
     'resource-fetch-algorithm': { func: videoref_helper, fragment: 'concept-media-load-resource', link_text: 'resource fetch algorithm',  },
     'intrinsic-width-and-height': { func: videoref_helper, fragment: 'concept-video-intrinsic-width', link_text: 'intrinsic width and height',  },
     'normalized-timeranges-object': { func: videoref_helper, fragment: 'normalized-timeranges-object', link_text: 'normalized TimeRanges object',  },
+    'current-playback-position': { func: videoref_helper, fragment: 'current-playback-position', link_text: 'current playback position',  },
     'media-data-is-corrupted': { func: videoref_helper, fragment: 'fatal-decode-error', link_text: 'media data is corrupted',  },
     'media-err-decode': { func: code_videoref_helper, fragment: 'dom-mediaerror-media_err_decode', link_text: 'MediaError.MEDIA_ERR_DECODE',  },
     'media-src': { func: code_videoref_helper, fragment: 'attr-media-src', link_text: 'src',  },
